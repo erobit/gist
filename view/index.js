ae0665e02ca7ae6af3e1802bba96662cabd8263d
Eric Robitaille <eric@vianet.ca>
2013-06-25 23:03:21 -0400

    updated view - foshizzle

--- view/index.js
+++ view/index.js
@@ -4,12 +4,5 @@ module['exports'] = function(options, callback) {
   var resource = require('resource');
   var r = resource.use('gist');
 
-  // present an informative index of all the gists
-  r.view.index.present(options, function(err, result) {
-    if (err) { 
-      $('#error').append('<pre>' + err.stack + '</pre>'); 
-    }
-    $('#main').html(result);
-    return callback(null, $.html());
-  });
+  return callback(null, 'foshizzle');
 };
