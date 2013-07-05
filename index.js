e79d2e7023de9b662bbbf7ea23cd3638aad00a2e
Eric Robitaille <eric@vianet.ca>
2013-07-05 19:45:28 -0400

    addd http resource

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var resource = require('resource');
 var gist = resource.define('gist');
+var http = resource.use('http');
 
 gist.schema.description = 'a resource for creating gists'
 
@@ -7,20 +8,20 @@ gist.schema.description = 'a resource for creating gists'
 // gist property definitions
 
 gist.property('description', { 
-	"description": "gist description",
-	"type": "string"
+  "description": "gist description",
+  "type": "string"
 });
 
 gist.property('public', {
-	"description": "gist privacy setting",
-	"type": "boolean",
-	"required": true
+  "description": "gist privacy setting",
+  "type": "boolean",
+  "required": true
 });
 
 gist.property('files', {
-	"description": "files that make up this gist",
-	"type": "object",
-	"required": true
+  "description": "files that make up this gist",
+  "type": "object",
+  "required": true
 });
 
 
@@ -28,35 +29,34 @@ gist.property('files', {
 // > https://github.com/bigcompany/resource/issues/21
 
 var start = function(options, callback) {
-	var async = require('async');
-	async.parallel([
+  var async = require('async');
+  async.parallel([
     // setup .view convention
     function(callback) {
       var view = resource.use('view');
       view.create({ path: __dirname + '/view' }, function(err, _view) {
           if (err) { return callback(err); }
           creature.view = _view;
-          http.app.use(view.middle({view: _view, prefix: 'creature'}));
+          http.app.use(view.middle({view: _view, prefix: 'gist'}));
           return callback(null);
       });
     },
-		// add gists property to user resource
-		function(callback) {
-			var user = resource.use('user');
-			user.property('gists', {
-				"description": "user gists",
-				"type": "array",
-				"items": {
-					"type": "string",
-					"description": "gist id"
-				},
-				"default": []
-			});
-			user.persist('memory');
-			return callback(null);
-		}], callback);
+    // add gists property to user resource
+    function(callback) {
+      var user = resource.use('user');
+      user.property('gists', {
+        "description": "user gists",
+        "type": "array",
+        "items": {
+          "type": "string",
+          "description": "gist id"
+        },
+        "default": []
+      });
+      user.persist('memory');
+      return callback(null);
+    }], callback);
 }
-
 
 // gist method definitions 
 // > http://ajaxorg.github.io/node-github/#gists
@@ -82,7 +82,7 @@ gist.persist('memory');
 
 
 gist.dependencies = {
-	'async': '*'
+  'async': '*'
 };
 
 exports.gist = gist;
